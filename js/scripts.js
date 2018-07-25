a7610e5f50e1110057259c477570a9986beaf579
Radek Izdebski <radek1izdebski@gmail.com>
2018-07-25 16:07:21 +0200

    Add script js for smooth scrolling navigation

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,34 @@
 //navigation variables
 const menuButton = document.getElementById('hamburger');//responsive menu
 
+let aboutMeMenu = document.getElementById('1');
+let aboutMeTarget = document.getElementById('1-target');
+let aboutThisPage = document.getElementById('2');
+let aboutThisPageTarget = document.getElementById('2-target');
+let galleryMenu = document.getElementById('3');
+let galleryTarget = document.getElementById('3-target');
+let contactMenu = document.getElementById('4');
+let contactTarget = document.getElementById('4-target');
 
 //responsive menu: switching view
 menuButton.addEventListener('click', function() {
     document.getElementById("menu").classList.toggle("show");
     return false;
 });
+
+//navigation: smooth scrolling
+aboutMeMenu.addEventListener('click', function() {
+	aboutMeTarget.scrollIntoView({behavior: 'smooth'});
+});
+aboutThisPage.addEventListener('click', function() {
+	aboutThisPageTarget.scrollIntoView({behavior: 'smooth'});
+});
+galleryMenu.addEventListener('click', function() {
+	galleryTarget.scrollIntoView({behavior: 'smooth'});
+});
+contactMenu.addEventListener('click', function() {
+	contactTarget.scrollIntoView({behavior: 'smooth'});
+});
+
+
 
